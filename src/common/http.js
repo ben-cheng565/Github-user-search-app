078d2607f2e7cb591d227b8a8b45e0287b2c4cbd
Ben <xche565@aucklanduni.ac.nz>
2022-08-20 10:00:16 +1200

    feat: Create fetcher function to fetch api data

--- src/common/http.js
+++ src/common/http.js
@@ -1,9 +1,20 @@
 import axios from "axios";
+import { notification } from "antd";
 
 const http = axios.create({
   headers: {
     "X-Requested-With": "XMLHttpRequest",
   },
 });
+
+export const fetcher = (url) =>
+  http
+    .get(url)
+    .then((res) => res.data)
+    .catch(() => {
+      notification.error({
+        message: "Username is invalid.",
+      });
+    });
 
 export default http;
